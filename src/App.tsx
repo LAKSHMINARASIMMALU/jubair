cce3b5311533217e1a4bb7cb638291d46c3fe22c
LAKSHMINARASIMMALU <134133194+LAKSHMINARASIMMALU@users.noreply.github.com>
2025-08-08 19:11:04 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -56,7 +56,7 @@ Kajamoideen J & Jamila K
             </div>
             
             <h2 className="text-4xl md:text-6xl font-bold bg-gradient-to-r from-purple-700 via-pink-700 to-rose-700 bg-clip-text text-transparent mb-3 elegant-font">
-              Faridh Khan
+              Farith Khan
             </h2>
             <div className="flex items-center justify-center my-4">
               <div className="w-20 h-1 bg-gradient-to-r from-transparent via-pink-500 to-transparent rounded-full"></div>
